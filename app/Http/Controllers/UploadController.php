e48b78e35eeadeac4cf08845b9cf8df4a18e1779
松岡聖人 <access.06030802@gmail.com>
2024-05-26 17:15:33 +0000

    picture fix

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,7 +18,7 @@ class UploadController extends Controller
         $datum['user_id'] = Auth::id();
 
         $file = $request->file('photo');
-        $path = Storage::disk('s3')->put('/', $file, 'public');
+        $path = Storage::disk('s3')->putFile('/photos', $file, 'public');
         $URLpath = Storage::disk('s3')->url($path);
         $datum['photo'] = $URLpath;
         $datum['prefecture'] = $name;
